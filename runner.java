36cdcab5d9fafaffba110c84aa6ecf0a562d324d
rhutujarajuskar <91114804+rhutujarajuskar@users.noreply.github.com>
2022-06-27 15:39:35 +0530

    Add files via upload

---
+++ runner.java
@@ -0,0 +1,18 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "src/test/java/feature",
+		glue = {"stepdefinition"},
+		monochrome = true
+		)
+
+public class runner {
+
+}
